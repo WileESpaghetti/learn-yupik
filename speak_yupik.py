2007e4bbd2e269be003315aa81d769bd90d24d83
WileESpaghetti <dosnlinux@gmail.com>
2012-10-13 17:30:29 -0500

    work to change citation for (-uq) to base form
    doesn't handle irregulars or invalid words.

--- speak_yupik.py
+++ speak_yupik.py
@@ -3,6 +3,10 @@ import random
 
 ylist_file = "yupik.txt"
 elist_file = "english.txt"
+uqwords = "uq-test-words.txt"
+
+yvowels = ['a', 'e', 'i', 'u'] #FIXME handle dbl letters and 'sometimes vowels'
+yconsonants = ['c', 'g', 'gg', 'k', 'l', 'll', 'm', 'n', 'ng', 'p', 'q', 'r', 'rr', 's', 'ss', 't', 'v', 'vv', 'w', 'y']
 
 def load_wordlists(yupik, english):
 	ylist = open(yupik)
@@ -63,12 +67,60 @@ def prompt(wordlist):
 
 wordlist = load_wordlists(ylist_file, elist_file)
 
-#print_list_files()
-#debug_dump()
-#choose_word(wordlist)
-while wordlist != []:
-	prompt(wordlist)
-#debug_dump()
-# prompt
-#input = raw_input('Please print hello: ')
-#print(input)
+# flash cards
+def flashcards():
+	while wordlist != []:
+		prompt(wordlist)
+
+def isVowel(c):
+	vowel = False
+	for v in yvowels:
+		if c == v:
+			vowel = True
+			break
+	return vowel
+
+def citation_suffix_type(word):
+	type = ''
+
+	if isVowel(word[-3]):
+		type = 'Vuq'
+	elif isVowel(word[-4]) and isVowel(word[-5]):
+		type = 'VVguq'
+	elif word[-3] == '\'':
+		type = '\'uq'
+	elif word[-3] == 't':
+		if word[-4] == 'g' and word[-5] != 'n':
+			 type = 'gtuq'
+		elif word[-4] == 'r':
+			type = 'rtuq'
+		else:
+			type = 'Cuq'
+	else:
+		type = 'Cuq!' #does not account for invalid words $uq or xuq, etc.
+
+	return type
+
+def uq_to_base(word):
+	type = citation_suffix_type(word)
+
+	if type == 'gtuq' or type == 'rtuq' or type == 'VVguq':
+		word = word[:-3]
+		#print(word)
+	elif type == '\'uq':
+		word = word[:-3]
+		word = word + 'e'
+		#print(word)
+	elif type == 'Cuq':
+		word = word[:-2]
+		word = word + 'e'
+		#print(word)
+	return word
+		
+
+def uq_forms():
+	uqfile = open(uqwords)
+	for w in [x.strip() for x in uqfile.readlines()]:
+		uq_to_base(w)
+
+uq_forms()
