03362d044ced4d341bd5af2bfaca0ac662ca5e49
WileESpaghetti <dosnlinux@gmail.com>
2012-10-13 23:44:55 -0500

    added notes about function naming conventions

--- speak_yupik.py
+++ speak_yupik.py
@@ -1,4 +1,40 @@
 #!python
+
+# Function naming conventions
+# fps = first person singular
+# fpd = first person dual
+# fpp = first person plural
+# sps = second person singular
+# spd = second person dual
+# spp = second person plural
+# tps = third person singular
+# tpd = third person dual
+# tpp = third person plural
+
+# rules for endings
+# +	keeps final consonant of base
+# -	drops final consonent
+# ~	drops final 'e'
+# ÷	keeps strong final consonents
+# :	drops voiced velar consonents if between single vowels where first vowel is prime
+# '	causes gemination of final consonent if base in the form of (C)VCe-
+# @	???
+# - -	base drops final conconant + vowel preceeding it. (only for shortened forms of consonent dropping postbases beginning with 'li'
+# %	attaches irregularly (certain non-productive postbases only)
+
+
+# TODO technically all of these begin with +(g/t)u, but we are not set up to do the kind of
+# parsing necessary to work with postbases in the 'official manner' yet
+fps_ending = ':nga'
+fpd_ending = 'kuk'
+fpp_ending = 'kut'
+sps_ending = 'ten'
+spd_ending = 'tek'
+spp_ending = 'ci'
+tps_ending = 'uq'
+tpd_ending = 'uk'
+tpp_ending = 'ut'
+
 uqwords = "uq-test-words.txt"
 
 yvowels = ['a', 'e', 'i', 'u'] #FIXME handle dbl letters and 'sometimes vowels'
@@ -38,15 +74,12 @@ def uq_to_base(word):
 
 	if type == 'gtuq' or type == 'rtuq' or type == 'VVguq':
 		word = word[:-3]
-		#print(word)
 	elif type == '\'uq':
 		word = word[:-3]
 		word = word + 'e'
-		#print(word)
 	elif type == 'Cuq':
 		word = word[:-2]
 		word = word + 'e'
-		#print(word)
 	return word
 		
 
