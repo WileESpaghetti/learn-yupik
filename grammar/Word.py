0f812badecb7b2bf32dec4c7db6c75740b9113a8
WileESpaghetti <dosnlinux@gmail.com>
2012-11-24 11:12:28 -0600

    work on breaking words into syllables

--- grammar/Word.py
+++ grammar/Word.py
@@ -4,23 +4,67 @@ vowels = ['a', 'e', 'i', 'u']
 primeVowels = ['a', 'i', 'u']
 
 fricatives = ['v', 'l', 's', 'y', 'g', 'r', 'ug', 'ur', 'vv', 'll', 'ss', 'gg', 'rr', 'w', 'urr']
+alphabet = ['\'', 'a', 'c', 'e', 'g', 'gg', 'i', 'k', 'l', 'll', 'm', 'n', 'ng', 'p', 'q', 'r', 'rr', 's', 'ss', 't', 'u', 'v', 'vv', 'w', 'y']
 
 def explode(word):
 	""" split the word into it's letters. this makes working with letters made up
 	of more than one character (eg. 'll' or 'ng' a whole lot easier """
 	#FIXME should \' be treated as a separate letter or combined with the letter
 	# it is telling to geminate (eg. r') or completely omited in the output
-	pass
+	exploded = []
 
-def getSyllables(word)
-	""" return a list of the syllables that make up word """
-	pass
+	x = range(len(word))
+	for i in x:
+		c = word[i]
+		if i < len(word) - 1:
+			#FIXME this might pick up letters like 'uu'. this causes tuquuq to be split incorrectly
+			doubleLetters = c == 'g' or c == 'l' or c == 'n' or c == 'r' or c == 's' or c == 'v'
+			if (doubleLetters  and c == word[i+1]) or (c == 'n' and word[i+1] == 'g'):
+				exploded.append(c + word[i+1])
+				x.pop(2)		
+			else:
+				exploded.append(c)
+		else:
+			exploded.append(c)
+
+	return exploded
 
 def syllableMatches(syl, form):
 	""" eg. syllableMatches('rte', '[V]VCe')
 	capital 'V' or 'C' match vowels and consonants respectivly
 	letters in []'s shows optional letters. """
-	pass
+	for i in form:
+		if i == V:
+			pass
+
+def getSyllables(word):
+	""" return a list of the syllables that make up word """
+	syllables = []
+	syl = []
+	for i in range(len(word)):
+		c = word[i]
+		syl.append(c)
+		if i < len(word) - 1:
+			if not isVowel(c) and not isVowel(word[i+1]):
+				syllables.append(syl)
+				syl = []
+		#print(syl)
+	syllables.append(syl)
+	#print(syllables)
+	#print("\n\n")
+	syl = []
+	syl2 = []
+	for s in syllables:
+		for i in range(len(s)):
+			if not isVowel(s[i]) and (i > 0 and i < len(s) - 1):
+				if isVowel(s[i-1]) and isVowel(s[i+1]):
+					syl2.append(syl)
+					syl = []
+			syl.append(s[i])
+		syl2.append(syl)
+		syl = []
+	print(syl2)
+	print("\n\n")
 
 def isSyllable(syl):
 	""" tests if syl is a valid Yup'ik syllable """
@@ -55,7 +99,7 @@ def isVelar(c):
 	""" is c a velar """
 	pass
 
-def isFricative(c)
+def isFricative(c):
 	""" is c a fricative """
 	isF = False
 	for f in fricatives:
@@ -84,5 +128,15 @@ def isGeminated(word, c):
 	"""test to see if the charcter at pos c is geminated"""
 	pass
 
-def isVoiced(word, c)
+def isVoiced(word, c):
 	"""test position c to see if it is voiced"""
+
+
+#print(explode('alinguq'))
+#getSyllables(explode('alinguq'))
+#print(explode('tuquuq'))
+#getSyllables(explode('tuquuq'))
+#print(explode('arnaq'))
+#getSyllables(explode('arnaq'))
+#print(explode('arnaq'))
+#getSyllables(explode('angyalingaicugnarquq'))
