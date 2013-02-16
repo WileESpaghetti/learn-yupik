4959aa591b3093d711ccc6236b60391a4b870f51
WileESpaghetti <dosnlinux@gmail.com>
2013-02-16 15:21:10 -0600

    did some cleanup of some apostrophe purpose tests

--- tests/Test_ApostrophePurpose.py
+++ tests/Test_ApostrophePurpose.py
@@ -3,6 +3,7 @@
 import unittest
 import grammar
 
+
 class ApostrophePurposeTest(unittest.TestCase):
     # TODO might be good to do some permutations of letters and test splitting
     # TODO write some tests that are supposed to fail
@@ -20,115 +21,55 @@ class ApostrophePurposeTest(unittest.TestCase):
         for w in self.ngTestWords:
             self.assertEqual(grammar.Word.apostrophePurpose(w), grammar.Word.APOS_NG_SEPARATOR)
 
-        for w in self.devoicingTestWords:
-            self.assertNotEqual(grammar.Word.apostrophePurpose(w), grammar.Word.APOS_NG_SEPARATOR)
-
-        for w in self.geminationTestWords:
-            self.assertNotEqual(grammar.Word.apostrophePurpose(w), grammar.Word.APOS_NG_SEPARATOR)
-
-        for w in self.noGeminationTestWords:
-            self.assertNotEqual(grammar.Word.apostrophePurpose(w), grammar.Word.APOS_NG_SEPARATOR)
-
-        for w in self.stressTestWords:
-            self.assertNotEqual(grammar.Word.apostrophePurpose(w), grammar.Word.APOS_NG_SEPARATOR)
-
-        for w in self.shortenedTestWords:
-            self.assertNotEqual(grammar.Word.apostrophePurpose(w), grammar.Word.APOS_NG_SEPARATOR)
+        for c in [self.devoicingTestWords, self.geminationTestWords, self.noGeminationTestWords, self.stressTestWords,
+                  self.shortenedTestWords]:
+            for w in c:
+                self.assertNotEqual(grammar.Word.apostrophePurpose(w), grammar.Word.APOS_NG_SEPARATOR)
 
     def testDevoicing(self):
-        for w in self.ngTestWords:
-            self.assertNotEqual(grammar.Word.apostrophePurpose(w), grammar.Word.APOS_PREVENT_DEVOICING)
+        for c in [self.ngTestWords, self.geminationTestWords, self.noGeminationTestWords, self.stressTestWords,
+                  self.shortenedTestWords]:
+            for w in c:
+                self.assertNotEqual(grammar.Word.apostrophePurpose(w), grammar.Word.APOS_PREVENT_DEVOICING)
 
         for w in self.devoicingTestWords:
             self.assertEqual(grammar.Word.apostrophePurpose(w), grammar.Word.APOS_PREVENT_DEVOICING)
 
-        for w in self.geminationTestWords:
-            self.assertNotEqual(grammar.Word.apostrophePurpose(w), grammar.Word.APOS_PREVENT_DEVOICING)
-
-        for w in self.noGeminationTestWords:
-            self.assertNotEqual(grammar.Word.apostrophePurpose(w), grammar.Word.APOS_PREVENT_DEVOICING)
-
-        for w in self.stressTestWords:
-            self.assertNotEqual(grammar.Word.apostrophePurpose(w), grammar.Word.APOS_PREVENT_DEVOICING)
-
-        for w in self.shortenedTestWords:
-            self.assertNotEqual(grammar.Word.apostrophePurpose(w), grammar.Word.APOS_PREVENT_DEVOICING)
-
     def testGemination(self):
-        for w in self.ngTestWords:
-            self.assertNotEqual(grammar.Word.apostrophePurpose(w), grammar.Word.APOS_GEMINATION_MARKER)
-
-        for w in self.devoicingTestWords:
-            self.assertNotEqual(grammar.Word.apostrophePurpose(w), grammar.Word.APOS_GEMINATION_MARKER)
+        for c in [self.ngTestWords, self.devoicingTestWords, self.noGeminationTestWords, self.stressTestWords,
+                  self.shortenedTestWords]:
+            for w in c:
+                    self.assertNotEqual(grammar.Word.apostrophePurpose(w), grammar.Word.APOS_GEMINATION_MARKER)
 
         for w in self.geminationTestWords:
             self.assertEqual(grammar.Word.apostrophePurpose(w), grammar.Word.APOS_GEMINATION_MARKER)
 
-        for w in self.noGeminationTestWords:
-            self.assertNotEqual(grammar.Word.apostrophePurpose(w), grammar.Word.APOS_GEMINATION_MARKER)
-
-        for w in self.stressTestWords:
-            self.assertNotEqual(grammar.Word.apostrophePurpose(w), grammar.Word.APOS_GEMINATION_MARKER)
-
-        for w in self.shortenedTestWords:
-            self.assertNotEqual(grammar.Word.apostrophePurpose(w), grammar.Word.APOS_GEMINATION_MARKER)
-
     def testNoGemination(self):
-        for w in self.ngTestWords:
-            self.assertNotEqual(grammar.Word.apostrophePurpose(w), grammar.Word.APOS_PREVENT_GEMMINATION)
-
-        for w in self.devoicingTestWords:
-            self.assertNotEqual(grammar.Word.apostrophePurpose(w), grammar.Word.APOS_PREVENT_GEMMINATION)
-
-        for w in self.geminationTestWords:
-            self.assertNotEqual(grammar.Word.apostrophePurpose(w), grammar.Word.APOS_PREVENT_GEMMINATION)
+        for c in [self.ngTestWords, self.devoicingTestWords, self.geminationTestWords, self.stressTestWords,
+                  self.shortenedTestWords]:
+            for w in c:
+                self.assertNotEqual(grammar.Word.apostrophePurpose(w), grammar.Word.APOS_PREVENT_GEMMINATION)
 
         for w in self.noGeminationTestWords:
             self.assertEqual(grammar.Word.apostrophePurpose(w), grammar.Word.APOS_PREVENT_GEMMINATION)
 
-        for w in self.stressTestWords:
-            self.assertNotEqual(grammar.Word.apostrophePurpose(w), grammar.Word.APOS_PREVENT_GEMMINATION)
-
-        for w in self.shortenedTestWords:
-            self.assertNotEqual(grammar.Word.apostrophePurpose(w), grammar.Word.APOS_PREVENT_GEMMINATION)
-
     def testDistruptStress(self):
-        for w in self.ngTestWords:
-            self.assertNotEqual(grammar.Word.apostrophePurpose(w), grammar.Word.APOS_DISRUPT_STRESS)
-
-        for w in self.devoicingTestWords:
-            self.assertNotEqual(grammar.Word.apostrophePurpose(w), grammar.Word.APOS_DISRUPT_STRESS)
-
-        for w in self.geminationTestWords:
-            self.assertNotEqual(grammar.Word.apostrophePurpose(w), grammar.Word.APOS_DISRUPT_STRESS)
-
-        for w in self.noGeminationTestWords:
-            self.assertNotEqual(grammar.Word.apostrophePurpose(w), grammar.Word.APOS_DISRUPT_STRESS)
+        for c in [self.ngTestWords, self.devoicingTestWords, self.geminationTestWords, self.noGeminationTestWords,
+                  self.shortenedTestWords]:
+            for w in c:
+                self.assertNotEqual(grammar.Word.apostrophePurpose(w), grammar.Word.APOS_DISRUPT_STRESS)
 
         for w in self.stressTestWords:
             self.assertEqual(grammar.Word.apostrophePurpose(w), grammar.Word.APOS_DISRUPT_STRESS)
 
-        for w in self.shortenedTestWords:
-            self.assertNotEqual(grammar.Word.apostrophePurpose(w), grammar.Word.APOS_DISRUPT_STRESS)
-
     def testShortWords(self):
-        for w in self.ngTestWords:
-            self.assertNotEqual(grammar.Word.apostrophePurpose(w), grammar.Word.APOS_SHORT_WORD)
-
-        for w in self.devoicingTestWords:
-            self.assertNotEqual(grammar.Word.apostrophePurpose(w), grammar.Word.APOS_SHORT_WORD)
-
-        for w in self.geminationTestWords:
-            self.assertNotEqual(grammar.Word.apostrophePurpose(w), grammar.Word.APOS_SHORT_WORD)
-
-        for w in self.noGeminationTestWords:
-            self.assertNotEqual(grammar.Word.apostrophePurpose(w), grammar.Word.APOS_SHORT_WORD)
-
-        for w in self.stressTestWords:
-            self.assertNotEqual(grammar.Word.apostrophePurpose(w), grammar.Word.APOS_SHORT_WORD)
+        for c in [self.ngTestWords, self.devoicingTestWords, self.geminationTestWords, self.noGeminationTestWords, self.stressTestWords]:
+            for w in c:
+                self.assertNotEqual(grammar.Word.apostrophePurpose(w), grammar.Word.APOS_SHORT_WORD)
 
         for w in self.shortenedTestWords:
             self.assertEqual(grammar.Word.apostrophePurpose(w), grammar.Word.APOS_SHORT_WORD)
+
 
 if __name__ == '__main__':
     unittest.main()
